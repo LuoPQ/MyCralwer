e1eb8b1ad5a321f6a7a024e0f58b4ee53ccf213a
lrzw32@126.com <lrzw32@126.com>
2014-03-27 18:32:15 +0800

    增加IP

---
+++ ProxyCollector/Entities/IPInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyCollector.Entities {
+    public class IPInfo {
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public AnonymousRank AnonymousRank { get; set; }
+        public string Region { get; set; }
+    }
+
+    public enum AnonymousRank {
+        Transparent,
+        Anonymous,
+        High
+    }
+}
---
+++ ProxyCollector/Helpers/ProxyHtmlHelper.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using ProxyCollector.Entities;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrapySharp.Extensions;
+
+namespace ProxyCollector.Helpers {
+    public static class ProxyHtmlHelper {
+        private static List<IPInfo> GetIpList(string url) {
+            List<IPInfo> ipInfoList = new List<IPInfo>();
+            var html = new ScrapingBrowser().DownloadString(new Uri(url));
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var htmlNode = htmlDocument.DocumentNode;
+            //htmlNode.CssSelect()
+            return null;
+        }
+    }
+}
